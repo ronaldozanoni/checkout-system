f19a404eee3fbdeca1a2e04faec1b2c290f83f8b
Leonardo Rossi <lrossi.dev@pm.me>
2022-02-20 18:08:41 -0300

    Project creation (using spring initializr)

---
+++ src/main/java/quikserve/challenge/promotions/PromotionsApiApplication.java
@@ -0,0 +1,13 @@
+package quikserve.challenge.promotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quikserve/challenge/promotions/PromotionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package quikserve.challenge.promotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
