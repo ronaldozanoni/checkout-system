1fd44128174698465a6daa283a65c7ce493a9b01
Leonardo Rossi <lrossi.dev@pm.me>
2022-02-23 19:58:00 -0300

    Initializing checkout-api

---
+++ checkout-api/src/main/java/qikserve/challenge/checkout/CheckoutApiApplication.java
@@ -0,0 +1,13 @@
+package qikserve.challenge.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutApiApplication.class, args);
+	}
+
+}
---
+++ checkout-api/src/test/java/qikserve/challenge/checkout/CheckoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package qikserve.challenge.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
